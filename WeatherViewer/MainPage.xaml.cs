3f9ef73c7dce0766632728d02511b648b09fbdab
Zaun14 <zaunmasters@gmail.com>
2022-11-11 21:59:37 +0200

    refactoring

--- WeatherViewer/MainPage.xaml.cs
+++ WeatherViewer/MainPage.xaml.cs
@@ -1,52 +1,63 @@
-﻿
-using System.Collections;
-
-namespace WetherViewer;
+﻿namespace WetherViewer;
 
 public partial class MainPage : ContentPage
 {
-  
-    public MainPage()
-    {
-        InitializeComponent();
-        BindingContext = this;
-        getCityP.SelectedIndex = 0;
-    }
-
-    public List<string> Citys { get; set; } = new List<string>()
+    private Picker _cityPicker;
+    private ActivityIndicator _weatherLoadingIndicator;
+    private Label _temperatureLabel;
+    private Button _weatherButton;
+    private Entry _countryEntry;
+    public List<string> Citys {get; set;} = new List<string>()
     {
         "London",
         "Paris",
         "Kyiv",
     };
 
-    private async void OnClickGetWeather(object sender, EventArgs e)
-    {  
-        var runningState = weatherLoadingIndicator.IsRunning;
-        weatherLoadingIndicator.IsRunning = !runningState;
-        getWeatherBtn.IsVisible = false;
-        var testTemperatureValue = await GetRandomValue();
-        temperatureValue.Text = testTemperatureValue.ToString() + "°C";
-        getWeatherBtn.IsVisible = true;
-        weatherLoadingIndicator.IsRunning = false;
-        
-    }
-    //unfocus if you press enter
-    private void OnCompletedCountryE(object sender, EventArgs e)
+    public MainPage()
     {
-        Country.Unfocus();
+        _cityPicker = CityPicker;
+        _weatherLoadingIndicator = WeatherLoadingIndicator;
+        _temperatureLabel = TemperatureLabel;
+        _weatherButton = WeatherButton;
+        _countryEntry = CountryEntry;
+        InitializeComponent();
+        BindingContext = this;
+        _cityPicker.SelectedIndex = 0;
     }
 
-    private void OnChangeTextConutryE(object sender , TextChangedEventArgs e) 
+
+    private async void OnClickWeatherButton(object sender, EventArgs e)
+    {  
+        _weatherLoadingIndicator.IsRunning = true;
+        _weatherLoadingIndicator.IsVisible = false;
+        var Temperature = await GetRandomValue();
+        _temperatureLabel.Text = Temperature.ToString() + "°C";
+        _weatherButton.IsVisible = true;
+        _weatherLoadingIndicator.IsRunning = false;
+    }
+
+    private void OnCompletedCountryEntry(object sender, EventArgs e)
     {
-        getCityP.IsEnabled = Country.Text != "";
+        ((Entry)sender).Unfocus();
+    }
+
+    private void OnChangeTextCountryEntry(object sender , TextChangedEventArgs e) 
+    {
+        var isEnabled = _countryEntry.Text != string.Empty;
+
+        _cityPicker.IsEnabled = isEnabled;
+        if (isEnabled) 
+        {
+            //TODO:main logic get citys
+        }
     }
 
         private async Task<int> GetRandomValue()
     {
         return await Task.Run(() =>
         {
-            //call getWeather
+            //TODO:call getWeather
             var rnd = new Random();
             Thread.Sleep(3000);
             return rnd.Next(0, 50);
